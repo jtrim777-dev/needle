55339898923d0d620d61cf155b9fa210c49ff167
Jake Trimble <jatrimble777@gmail.com>
2023-03-28 18:30:53 -0400

    Allow DelayedRegistry to look int un-init list

--- src/main/scala/dev/jtrim777/needle/registry/DelayedRegistry.scala
+++ src/main/scala/dev/jtrim777/needle/registry/DelayedRegistry.scala
@@ -5,6 +5,7 @@ import net.minecraft.util.registry.Registry
 import org.apache.logging.log4j.{LogManager, Logger}
 
 import scala.reflect.ClassTag
+import scala.util.{Failure, Success, Try}
 
 abstract class DelayedRegistry[T : ClassTag] {
   def registry: Registry[T]
@@ -42,6 +43,13 @@ abstract class DelayedRegistry[T : ClassTag] {
 
     if (registry.containsId(ident)) {
       registry.get(ident)
+    } else if (entries.contains(id)) {
+      Try(entries(id)()) match {
+        case Failure(exception) =>
+          log.warn(s"Tried to access $entryClassName '$namespace:$id' before it could be initialized; Initialization failed w/ $exception")
+          throw new NoSuchElementException(s"No such $entryClassName '$namespace:$id' in registry ${registry.getKey.toString} (entry has not yet been registered)")
+        case Success(value) => value
+      }
     } else {
       throw new NoSuchElementException(s"No such $entryClassName '$namespace:$id' in registry ${registry.getKey.toString}")
     }
