a68b18bddf09c8657b222ccd9d27779f82a8c661
Jake Trimble <jatrimble777@gmail.com>
2023-04-03 13:10:09 -0400

    Disable preload-lookup

--- src/main/scala/dev/jtrim777/needle/registry/DelayedRegistry.scala
+++ src/main/scala/dev/jtrim777/needle/registry/DelayedRegistry.scala
@@ -43,15 +43,16 @@ abstract class DelayedRegistry[T : ClassTag] {
 
     if (registry.containsId(ident)) {
       registry.get(ident)
-    } else if (entries.contains(id)) {
+    } /*else if (entries.contains(id)) {
       Try(entries(id)()) match {
         case Failure(exception) =>
           log.warn(s"Tried to access $entryClassName '$namespace:$id' before it could be initialized; Initialization failed w/ $exception")
           throw new NoSuchElementException(s"No such $entryClassName '$namespace:$id' in registry ${registry.getKey.toString} (entry has not yet been registered)")
         case Success(value) => value
       }
-    } else {
-      throw new NoSuchElementException(s"No such $entryClassName '$namespace:$id' in registry ${registry.getKey.toString}")
+    }*/ else {
+      val extraMessage = if (entries.contains(id)) " (entry has not yet been registered)" else ""
+      throw new NoSuchElementException(s"No such $entryClassName '$namespace:$id' in registry ${registry.getKey.toString}$extraMessage")
     }
   }
 
